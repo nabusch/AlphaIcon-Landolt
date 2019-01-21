e37af2f6fb946ae8349616ca2d5f0ecddf4ab2a9
nabusch <niko.busch@gmail.com>
2019-01-21 17:41:57 +0100

    plot show empirical results and parameter estimates

--- my_plot_qpr.m
+++ my_plot_qpr.m
@@ -1,30 +1,59 @@
+%% Load results file.
+clear; close all
+name = 'test3';
+logfilename = ['./Logfiles' filesep name '_Logfile.mat'];
+load(logfilename);
 
-T = INFO.T;
-soas = unique([T.soa]);
+%% Prepare data to plot.
 
-for isoa = 1:length(soas)
-    
-    thissoa = soas(isoa);
-    n(isoa) = sum([T.soa]==thissoa);
-    pc(isoa) = sum([T.soa]==thissoa & [T.correct]) / n(isoa);
-    n(isoa) = sum([T.soa]==thissoa);
+for isetsize = 1:length(INFO.P.stim.set_size)
+    for icontrast = 1:size(INFO.P.stim.target_color,1)
+        
+        thissetsize  = INFO.P.stim.set_size(isetsize);
+        thiscontrast = INFO.P.stim.target_color(icontrast);
+        trials = [INFO.T.setsize]' == thissetsize & all(vertcat(INFO.T.target_color) == thiscontrast,2);
+        
+        T = INFO.T(trials);
+        soas = unique([T.soa]);
+        
+        for isoa = 1:length(soas)
+            % how many trials at this SOA?
+            n(isoa,isetsize,icontrast) = sum([T.soa]==soas(isoa));
+            
+            % proportion of correct trials at this SOA
+            pc(isoa,isetsize,icontrast) = mean([T([T.soa]==soas(isoa)).correct]);
+        end
+        
+        Q = INFO.QPR;
+    end
 end
 
-figure;hold all
-plot(soas, pc, 'o')
 
-tData = q.sim.decayfnc;
+Q.this = qEstimate(Q.this, Q.setting);
 
 
-q = INFO.QPR;
-% PF
-soa4plot = sort([q.setting.soa q.setting.StimDur]);
 
-iPC = q.this.DecayFnc([1 1:end]);
+
+% Reconstruct psychometric function based on parameter estimates.
+tData = Q.sim.decayfnc;
+soa4plot = sort([Q.setting.soa Q.setting.StimDur]);
+iPC = Q.this.DecayFnc([1 1:end]);
+
+%% Show results using qPR's own plot function.
+qPlot(Q.setting, Q.sim, Q.this, Q.hist)
+
+%% Use my own visualization and make sure that it matches,
+figure('color', 'w');hold all
+
+% Show proportion correct at each SOA.
+scatter(soas, pc, 'o', 'filled')
+
+% Show the decay function estimated from the empirical data.
 plot(soa4plot,iPC,'k','LineWidth',2);
-plot(soa4plot,tData([1 1:end]),'r','LineWidth',2);hold on;
-legend('empirical', 'apriori')
 
-%%
+% Show the apriori decayfunction.
+plot(soa4plot,tData([1 1:end]),'r','LineWidth',2);
 
-qPlot(q.setting,q.sim,q.this,q.hist)
+legend('pc and n trials', 'apriori', 'empirical')
+
+text(soas, pc, int2str(n'))
