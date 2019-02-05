6b55bb2a0700f0ab42a6c773b727858c505d1f69
nabusch <niko.busch@gmail.com>
2019-02-05 11:28:49 +0100

    moving code for drawing the display to a separate code snippet

---
+++ Functions/draw_display.m
@@ -0,0 +1,13 @@
+draw_display
+
+l = lines(P.stim.set_size);
+l = Shuffle(l);
+
+for ipos = 1:P.stim.set_size      
+%     P.stim.target_gapcolor.*T.target_color
+    
+    my_landolt(win, xcoords(ipos) + P.screen.cx, ycoords(ipos) + P.screen.cy, ...
+        P.stim.target_diameter*P.screen.pixperdeg, T.target_color, P.stim.target_thick*P.screen.pixperdeg, ...
+        P.stim.target_gapcolor, P.stim.target_gap, ...
+        T.orientations(ipos)-0.5*P.stim.target_gap);    
+end
